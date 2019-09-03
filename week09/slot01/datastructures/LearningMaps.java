09f93f8b2bc20128e2c8eed37ca501c917c7d291
Jailani Abdul Rahman <jailani.har@googlemail.com>
2019-09-03 09:33:20 +0800

    week09 codes

--- week09/slot01/datastructures/LearningMaps.java
+++ week09/slot01/datastructures/LearningMaps.java
@@ -1,5 +1,55 @@
 package week09.slot01.datastructures;
 
+import java.util.*;
+
 public class LearningMaps {
 
+	public static void main(String[] args) {
+		HashMap<String, String> students =
+				new HashMap<String, String>();
+//		LinkedHashMap<String, String> students =
+//				new LinkedHashMap<String, String>();
+//		TreeMap<String, String> students =
+//				new TreeMap<String, String>();
+		
+		students.put("01765432", "Abdul");
+		students.put("01327777", "Rahman");
+		students.put("00123789", "Jailani");
+		students.put("51233445", "Mohammad");
+		System.out.println(students);
+		
+		if(!students.containsKey("51233445")) {
+			students.put("51233445", "Md");
+			System.out.println(students);
+		}
+		
+		students.put("51888999", "Jailani");
+		System.out.println(students);
+		
+		System.out.println(students.get("01765432"));
+		
+		students.remove("51888999");
+		System.out.println(students);
+		
+		students.remove("51233445", "Mohd");
+		System.out.println(students);
+		
+		students.replace("01765432", "Abd");
+		System.out.println(students);
+		
+		students.replace("01765987", "Abd");
+		System.out.println(students);
+		
+		students.replace("00123789", "Jai", "Haji");
+		System.out.println(students);
+		
+		System.out.println(students.keySet());
+		System.out.println(students.values());
+	}
+	
 }
+
+
+
+
+
--- week09/slot01/datastructures/LearningSets.java
+++ week09/slot01/datastructures/LearningSets.java
@@ -1,5 +1,30 @@
 package week09.slot01.datastructures;
 
+import java.util.*;
+
 public class LearningSets {
 
+	public static void main(String[] args) {
+//		HashSet<String> icNumber = new HashSet<String>();
+//		LinkedHashSet<String> icNumber = 
+//				new LinkedHashSet<String>();
+		TreeSet<String> icNumber = new TreeSet<String>();
+		
+		icNumber.add("51345678");
+		icNumber.add("00123456");
+		icNumber.add("00123456");
+		icNumber.add("01987654");
+		System.out.println(icNumber);
+		
+		System.out.println(icNumber.contains("00123456"));
+		System.out.println(icNumber.contains("01123456"));
+		
+		icNumber.remove("00123456");
+		System.out.println(icNumber);
+		
+		for(String e : icNumber) {
+			System.out.println(e);
+		}
+	}
+	
 }
